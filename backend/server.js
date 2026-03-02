5bfa212f3005e8d0c54eb74548dcfdef081a917d
steffanperera <steffanperera1997@gmail.com>
2026-03-02 14:40:22 +1100

    update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("api/notes", (req, res) => {
+  res.send("New Notes!");
+});
+
 app.listen(5001, () => {
   console.log("PORT: 5001");
 });
